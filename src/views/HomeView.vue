f7d50363b16293aafe5f2c232a62ef9b7dd556e6
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-01-29 17:19:48 +0100

    🎨 / installe des dépendances

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ColorPicker } from 'primevue';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ColorPicker v-model="color" />
   </main>
 </template>

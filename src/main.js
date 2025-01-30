08ab5791c4221efd8a652d04849d30a78ddb43de
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-01-30 16:34:28 +0100

    🚧 /wip

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import'./assets/main.css';
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
@@ -7,6 +9,7 @@ import Aura from '@primevue/themes/aura';
 
 import App from './App.vue'
 import router from './router'
+import { updatePrimaryPalette } from '@primevue/themes';
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <script setup>
+import NavbarComponnent from '@/components/NavbarComponnent.vue';
+import HomeComponnent from '@/components/NewFormComponnent.vue';
+import { ref } from 'vue';
+import { RouterView, RouterLink } from 'vue-router';
+
+
 
 
 </script>
 
 <template>
+  <NavbarComponnent />
+  <HomeComponnent />
+  
   <main>
-    
 
   </main>
 </template>

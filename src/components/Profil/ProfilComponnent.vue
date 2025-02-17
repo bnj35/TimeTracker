c46bf88ec5848af958ba0385e8bc8b1307ef9ab3
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-02-17 13:22:08 +0100

    :bricks: enhance ProfilComponent with user profile update functionality; add toast notifications for success and error handling; adjust layout in ProfilWindowComponent

--- src/components/Profil/ProfilComponnent.vue
+++ src/components/Profil/ProfilComponnent.vue
@@ -1,15 +1,35 @@
 <script setup>
+import { useUserStore } from '@/stores/userStore';
+import { ref } from 'vue';
+import { Form } from '@primevue/forms';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const userStore = useUserStore();
+
+const user = ref(userStore.user);
+
+function updateUser() {
+    userStore.UpdateProfile(user.value.name, user.value.mail);
+}
 
 </script>
 
 <template>
     <div class="p-grid p-fluid w-full h-full">
         <div class="p-col-12">
-            <div class="card">
-                <h1>Profil</h1>
-                <p>Vous pouvez modifier vos informations personnelles ici.</p>
+            <div class="p-grid p-fluid flex flex-col space-y-3">
+                <h1 class="text-primary-950 text-4xl">Bonjour {{ user.name }}</h1>
+                <p class="text-primary-800 ">Vous pouvez modifier vos informations personnelles ici.</p>
+                <Form @submit="updateUser" class="p-fluid p-grid flex flex-col space-y-3">
+                    <label for="mail">mail</label>
+                    <InputText v-model="user.mail"/>
+                    <label for="name">name</label>
+                    <InputText v-model="user.name"/>
+
+                    <Button type="submit" severity="primary" label="Modifier" class="w-fit-content"/>
+                </Form>
             </div>
         </div>
     </div>
-    
 </template>
--- src/components/Profil/ProfilWindowComponent.vue
+++ src/components/Profil/ProfilWindowComponent.vue
@@ -6,7 +6,7 @@ import Panel from 'primevue/panel';
 </script>
 
 <template>
-    <Panel class="flex flex-col gap-4  w-full h-full">
+    <Panel class="flex flex-row gap-4  w-full h-full">
 
         <ProfilComponnent />
         <StatsComponnent />

b08ad558be4d2b29a459d16d7886cce56a169380
Antoine Rubeo-lisa <73071779+Maison-hub@users.noreply.github.com>
2025-02-19 13:52:35 +0100

    Revert ":sparkles: introduce time entries"
    This reverts commit fb63fba9ce40edcc26a21d26e556f70700a7132e.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="js">
 import { RouterView } from 'vue-router';
-import Toast from 'primevue/toast';
 
 </script>
 
 <template>
     <div>
-      <Toast />
-      <router-view />
+        <router-view />
     </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -9,9 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-      
-    }
   ],
 })
 
